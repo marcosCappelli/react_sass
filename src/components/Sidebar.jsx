015001fc9166546f877a892b1b953db1bea5c2b3
Marcoscappelli <cappelli.m@gmail.com>
2022-07-17 12:02:02 -0300

    Links sociais

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,15 @@
+import SocialNetworks from "./SocialNetworks"
+
 import Avatar from "../img/Marcos.jpg"
 
 import "../styles/components/sidebar.sass"
+
 
 const Sidebar = () => {
   return <aside id="sidebar">
     <img src={Avatar} alt="Marcos Cappelli" />
     <p className="title">Dev Front end</p>
-    <p>redes sociais</p>
+    <SocialNetworks />
     <p>informações de contato</p>
     <a href="" className="btn">Download currículo</a>
   </aside>
