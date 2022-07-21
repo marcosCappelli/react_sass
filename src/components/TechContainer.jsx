fc072f96fbd8feb0aff34a21ac039546b10b0b5a
Marcoscappelli <cappelli.m@gmail.com>
2022-07-21 12:19:06 -0300

    Criando page tech

--- src/components/TechContainer.jsx
+++ src/components/TechContainer.jsx
@@ -1,9 +1,10 @@
-import { DiHtml5 } from "react-icons/di"
+import { DiHtml5, DiCss3 } from "react-icons/di"
 
 import "../styles/components/technologiescontainer.sass"
 
 const technologies = [
-{id: "html", name:"HTML5", icon:<DiHtml5/>}
+{id: "html", name:"HTML5", icon:<DiHtml5/>},
+{id: "css", name:"CSS3", icon:<DiCss3/>}
 ]
 
 const TechContainer = () => {
@@ -12,11 +13,13 @@ const TechContainer = () => {
         <h2>Tecnologias</h2>
        <div className="technologies-grid">
         {technologies.map((tech) => (
-            <div className="technology-card" id={tech.id} key={tech.id}>{tech.icon}
+            <div className="technology-card" id={tech.id} key={tech.id}>
+            {tech.icon}
+            <div className="technology-info">
             <h3>{tech.name}</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-
-            </p>
+               <p>HTML5, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</p>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> 
+            </div>    
             </div>
         ))}
        </div>
